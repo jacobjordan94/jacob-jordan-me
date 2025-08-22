41ca827d6bfc92788f5399fc5760eff6801aab39
Jacob Jordan <jacobjordan94@live.com>
2025-08-22 18:44:22 -0500

    add globals file

---
+++ src/context/Globals.js
@@ -0,0 +1,8 @@
+const Globals = {
+    emailLink: 'mailto:jacobjordan94@live.com',
+    email: 'jacobjordan94@live.com',
+    phoneNumberLink: 'tel:+14327709334',
+    phoneNumber: '+1 432-770-9334'
+};
+
+export default Globals;
