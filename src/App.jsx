c9b80a7a230382b20e078d6386150137a4e60cd4
Jacob Jordan <jacobjordan94@live.com>
2025-08-22 18:44:41 -0500

    add global context

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import HomePage from './pages/Home'
 import StandardLayout from './layouts/StandardLayout'
 import { Header } from './components/Header';
+import { createContext, useState } from 'react';
+import Globals from './context/Globals';
+
+export const GlobalContext = createContext();
 
 function App() {
+  const [ globals ] = useState(Globals)
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route element={ <StandardLayout /> }>
-          <Route index element={ <HomePage /> }></Route>
-        </Route>
-      </Routes>
+      <GlobalContext value={ globals }>
+        <Header />
+        <Routes>
+          <Route element={ <StandardLayout /> }>
+            <Route index element={ <HomePage /> }></Route>
+          </Route>
+        </Routes>
+      </GlobalContext>
     </BrowserRouter>
   )
 }
