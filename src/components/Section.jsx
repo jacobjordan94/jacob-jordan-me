915e003f6728ad2a8de7c9a98b9b50b78470e4c4
Jacob Jordan <jacobjordan94@live.com>
2025-08-22 20:28:23 -0500

    updates to section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,8 @@
 export default function Section({ title, children }) {
     return (
-        <div className="section">
-            <div className="title">{ title }</div>
-            <div className="section-content">
+        <div className="section workbench-font lowercase p-2">
+            <div className="title text-2xl">{ title }</div>
+            <div className="section-content p-3">
                 { children }
             </div>
         </div>
