e6080d6831759cd850da9fea9ca7df067fbc9cfd
Jacob Jordan <jacobjordan94@live.com>
2025-08-22 20:24:42 -0500

    add paperairplaneicon

---
+++ src/components/icons/PaperAirplaneIcon.jsx
@@ -0,0 +1,19 @@
+export default ({ className = '', size=24 }) => 
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} className={ 'text-white ' + className }>
+  <g>
+    <path d="M19.424999999999997 8.5725H24v1.1400000000000001h-4.574999999999999Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M21.7125 10.86H24V12h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="m6.855 16.575000000000003 5.715 0 0 1.1400000000000001 5.715 0 0 -2.2874999999999996 3.4275 0 0 -2.2800000000000002 -1.1400000000000001 0 0 1.1400000000000001 -19.4325 0 0 -1.1400000000000001 -1.1400000000000001 0 0 2.2800000000000002 6.855 0 0 1.1475z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M18.285 12h2.2874999999999996v1.1475h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M17.137500000000003 6.285h2.2874999999999996v1.1475h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M15.997499999999999 10.86h2.2874999999999996V12h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="m13.71 9.712499999999999 -1.1400000000000001 0 0 1.1475 3.4275 0 0 -1.1475 -1.1400000000000001 0 0 -1.1400000000000001 -1.1475 0 0 1.1400000000000001z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="m14.857499999999998 8.5725 1.1400000000000001 0 0 -2.2874999999999996 -3.4275 0 0 1.1475 2.2874999999999996 0 0 1.1400000000000001z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M10.2825 7.4325h2.2874999999999996v1.1400000000000001h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M9.1425 10.86h3.4275V12h-3.4275Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M7.995 8.5725h2.2874999999999996v1.1400000000000001h-2.2874999999999996Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M5.715 9.712499999999999h2.2800000000000002v1.1475H5.715Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M3.4275 10.86h2.2874999999999996V12H3.4275Z" fill="currentColor" strokeWidth="0.75"></path>
+    <path d="M1.1400000000000001 12h2.2874999999999996v1.1475H1.1400000000000001Z" fill="currentColor" strokeWidth="0.75"></path>
+  </g>
+</svg>
