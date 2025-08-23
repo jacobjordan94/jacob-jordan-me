845ab228ea3f7dce004f7b2a8a98e13ddea7a32f
Jacob Jordan <jacobjordan94@live.com>
2025-08-22 20:47:49 -0500

    add menu icons

---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,19 @@
+export default ({ className = '', size = 24 }) =>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height={size} width={size} className={"text-white " + className}>
+        <g>
+            <path d="M22.2825 2.8575h1.1475v18.285h-1.1475Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M21.142500000000002 21.142500000000002h1.1400000000000001v1.1400000000000001h-1.1400000000000001Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M21.142500000000002 1.71h1.1400000000000001v1.1475h-1.1400000000000001Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M2.8575 22.2825h18.285v1.1475H2.8575Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="m18.855 15.4275 -13.71 0 0 1.1400000000000001 -1.1475 0 0 2.2874999999999996 1.1475 0 0 -1.1400000000000001 13.71 0 0 1.1400000000000001 1.1475 0 0 -2.2874999999999996 -1.1475 0 0 -1.1400000000000001z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="m18.855 9.712499999999999 -13.71 0 0 1.1400000000000001 -1.1475 0 0 2.2874999999999996 1.1475 0 0 -1.1400000000000001 13.71 0 0 1.1400000000000001 1.1475 0 0 -2.2874999999999996 -1.1475 0 0 -1.1400000000000001z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="m18.855 3.9975 -13.71 0 0 1.1400000000000001 -1.1475 0 0 2.2874999999999996 1.1475 0 0 -1.1400000000000001 13.71 0 0 1.1400000000000001 1.1475 0 0 -2.2874999999999996 -1.1475 0 0 -1.1400000000000001z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M5.1450000000000005 18.855h13.71v1.1400000000000001H5.1450000000000005Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M5.1450000000000005 13.14h13.71v1.1400000000000001H5.1450000000000005Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M5.1450000000000005 7.425000000000001h13.71v1.1475H5.1450000000000005Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M2.8575 0.5700000000000001h18.285v1.1400000000000001H2.8575Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M1.7175 21.142500000000002h1.1400000000000001v1.1400000000000001H1.7175Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M1.7175 1.71h1.1400000000000001v1.1475H1.7175Z" fill="currentColor" strokeWidth="0.75"></path>
+            <path d="M0.5700000000000001 2.8575h1.1475v18.285H0.5700000000000001Z" fill="currentColor" strokeWidth="0.75"></path>
+        </g>
+    </svg>
