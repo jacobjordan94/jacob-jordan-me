0f44d9e35d5b1c10892a94575cbc472b9b74b747
Jacob Jordan <jacobjordan94@live.com>
2025-08-23 02:36:06 -0500

    update color of about, add indent

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     const { about } = useContext(GlobalContext);
     return (
         <Section title="about">
-            <div className="about">
+            <div className="about text-neutral-400 indent-6">
                 { about }
             </div>
         </Section>
