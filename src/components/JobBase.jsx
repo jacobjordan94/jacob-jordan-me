6db6c746577b5fa58029d5f3a62725c389b4005c
Jacob Jordan <jacobjordan94@live.com>
2025-08-24 19:16:20 -0500

    create job base component

---
+++ src/components/JobBase.jsx
@@ -0,0 +1,29 @@
+import WorldWideWebIcon from "./icons/WorldWideWebIcon";
+
+export default function JobBase({ job, children }) {
+    return (
+        <div className="job">
+            <div className="information">
+                <div className="name text-2xl inline hover:border-b-2 hover:animate-pulse">
+                    <a className="inline-flex items-center gap-3" href={job.href}>
+                        <WorldWideWebIcon size="20" />
+                        <span className="uppercase flex">{ job.name }</span>
+                    </a>
+                </div>
+                <div className="ms-8">
+                    <div className="position text-neutral-400 text-xl">
+                        <span>{ job.position }</span>
+                    </div>
+                    <div className="location-dates">
+                        <span>{ job.location }</span>
+                        <span className="mx-2 text-xs">&bull;</span>
+                        <span>{ job.dates[0] } &mdash; { job.dates[1] }</span>
+                    </div>
+                </div>
+            </div>
+            <div className="more">
+                { children }
+            </div>
+        </div>
+    );
+}
