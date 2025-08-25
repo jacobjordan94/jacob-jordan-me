ce2e2500762c533b7a7ad84501cc56a24ea0a535
Jacob Jordan <jacobjordan94@live.com>
2025-08-24 19:17:01 -0500

    add jobs to experience page

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../App";
+import Job from "./Job";
+
 export default function ExperiencePage({}) {
+    const { experience } = useContext(GlobalContext);
     return (
-        <div className="experience-page">
-            page
+        <div className="experience-page flex flex-col gap-6">
+        {
+            experience.map((job, i) => <Job key={i} job={job} />)
+        }
         </div>
     );
 }
