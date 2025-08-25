ddbde9c96264afce1dc1f112629cb6d49dd14577
Jacob Jordan <jacobjordan94@live.com>
2025-08-24 19:16:47 -0500

    job component now uses job base component

--- src/pages/experience/Job.jsx
+++ src/pages/experience/Job.jsx
@@ -1,27 +1,7 @@
-import WorldWideWebIcon from "../../components/icons/WorldWideWebIcon";
+import JobBase from "../../components/JobBase";
 
 export default function Job({ job }) {
     return (
-        <div className="job">
-            <div className="name text-2xl inline hover:border-b-2">
-                <a className="inline-flex items-center gap-3" href={job.href}>
-                    <WorldWideWebIcon size="20" />
-                    <span className="uppercase flex">{ job.name }</span>
-                </a>
-            </div>
-            <div className="ms-8">
-                <div className="position text-neutral-400 text-xl">
-                    <span>{ job.position }</span>
-                </div>
-                <div className="location-dates">
-                    <span>{ job.location }</span>
-                    <span className="mx-2 text-xs">&bull;</span>
-                    <span>{ job.dates[0] } &mdash; { job.dates[1] }</span>
-                </div>
-                <div className="description">
-                    { job.description }
-                </div>
-            </div>
-        </div>
+        <JobBase job={job} />
     );
 }
