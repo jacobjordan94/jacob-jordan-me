43b7428f590c473a6a32588ba5da48c7b6da1d3d
Jacob Jordan <jacobjordan94@live.com>
2025-08-24 23:09:42 -0500

    remove extra word

--- src/context/Globals.js
+++ src/context/Globals.js
@@ -105,7 +105,7 @@ const Globals = {
             location: 'monroeville, pa',
             position: 'software engineer iii',
             dates: ['7/22', '5/23'], 
-            description: 'iq inc. is a software development company and consulting company that specializes in custom software development',
+            description: 'iq inc. is a software development and consulting company that specializes in custom software development',
             detailedDescription: `
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
                 Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
