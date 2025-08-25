3187e7918869ffca84b987bbb42c2cf2aaf9542f
Jacob Jordan <jacobjordan94@live.com>
2025-08-24 23:21:57 -0500

    add some responsive design to experience page

--- src/layouts/StandardLayout.jsx
+++ src/layouts/StandardLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function StandardLayout() {
     return (
-        <div className="page-standard workbench-font p-4">
+        <div className="page-standard workbench-font p-4 @container">
             <Outlet />
         </div>
     );
--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -5,10 +5,12 @@ import Job from "./Job";
 export default function ExperiencePage({}) {
     const { experience } = useContext(GlobalContext);
     return (
-        <div className="experience-page flex flex-col gap-6">
-        {
-            experience.map((job, i) => <Job key={i} job={job} />)
-        }
+        <div className="experience-page @7xl:max-w-1/2 @7xl:m-auto">
+            <div className="experience-container flex flex-col gap-6">
+            {
+                experience.map((job, i) => <Job key={i} job={job} />)
+            }
+            </div>
         </div>
     );
 }
