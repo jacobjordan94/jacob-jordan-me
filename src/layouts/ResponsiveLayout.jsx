5126319bac01e9cbb2b9b2bd7bdceb64d91c7356
Jacob Jordan <jacobjordan94@live.com>
2025-08-27 20:07:15 -0500

    add new layout pages

---
+++ src/layouts/ResponsiveLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+export default function ResponsiveLayout({}) {
+    return (
+        <div className="responsive-layout @container">
+            <div className="@6xl:max-w-2/3 @6xl:m-auto p-4 @6xl:p-0 @6xl:py-4">
+                <Outlet />
+            </div>
+        </div>
+    );
+}
