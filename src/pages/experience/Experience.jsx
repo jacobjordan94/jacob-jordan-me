2009b930c3a0e0d9ee81524e2c2d90eaaef258c3
Jacob Jordan <jacobjordan94@live.com>
2025-08-27 20:07:42 -0500

    remove responsive styling on experience page; let new layoput handle

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { GlobalContext } from "../../App";
-import Job from "./Job";
 import UPMCJob from './UPMCJob';
 import IQIncJob from './IQIncJob';
 import VerticalLifeJob from './VerticalLifeJob';
@@ -9,13 +8,11 @@ import AffiliatedFoodsJob from './AffiliatedFoodsJob';
 export default function ExperiencePage({}) {
     const { experience } = useContext(GlobalContext);
     return (
-        <div className="experience-page @6xl:max-w-2/3 @6xl:m-auto">
-            <div className="experience-container flex flex-col gap-6">
-                <UPMCJob job={experience[0]} />
-                <IQIncJob job={experience[1]} />
-                <VerticalLifeJob job={experience[2]} />
-                <AffiliatedFoodsJob job={experience[3]} />
-            </div>
+        <div className="experience-page flex flex-col gap-6">
+            <UPMCJob job={experience[0]} />
+            <IQIncJob job={experience[1]} />
+            <VerticalLifeJob job={experience[2]} />
+            <AffiliatedFoodsJob job={experience[3]} />
         </div>
     );
 }
