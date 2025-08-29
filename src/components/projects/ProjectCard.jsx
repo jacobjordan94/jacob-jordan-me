4ac35df9f327953bc3f9ce5414d2497812be2f42
Jacob Jordan <jacobjordan94@live.com>
2025-08-29 18:50:00 -0500

    updates to project card component

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,15 +1,26 @@
-export default function ProjectCard({ project, type, className = '' }) {
+import AppLink from "../AppLink";
+import LinkIcon from "../icons/LinkIcon";
+
+export default function ProjectCard({ project, type, className = '', showLink = false, detailed = false }) {
     return (
         <div className={"projects-card relative group shadow-black rounded-md overflow-hidden transition-[transform_shadow] hover:transform-[scale(1.025)] shadow-md hover:shadow-lg " + className}>
-            <div className={`image overflow-hidden h-[256px]`}>
+            <div className={`image overflow-hidden h-[256px] border-b-1 border-[rgba(255,255,255,0.25)]`}>
                 <img 
                     src={ project.image === '' ? 'https://placehold.co/600x400' : project.image } 
                     alt={`project image for ${project.title}`} 
-                    className="w-full h-full object-cover"
+                    className="w-full h-full transition-[width_height] object-cover group-hover:h-[125%] group-hover:w-[125%]"
                 />
             </div>
-            <div className="information p-2">
-                <div className="title text-xl">{ project.title }</div>
+            <div className="information px-4 py-3">
+                <div className="title-row flex justify-between">
+                    <div className="title text-xl">{ project.title }</div>
+                    {
+                        showLink && project.url &&
+                        <AppLink href={project.url} decoration={false}>
+                            <LinkIcon />
+                        </AppLink>
+                    }
+                </div>
                 <div className="description text-neutral-400">{ project.description }</div>
             </div>
             {
