59bc1ab490247596880ead9ad3c414f3ff3daa1d
Jacob Jordan <jacobjordan94@live.com>
2025-08-29 22:13:28 -0500

    update styling of project card

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -8,7 +8,7 @@ export default function ProjectCard({ project, type, className = '', showLink = 
                 <img 
                     src={ project.image === '' ? 'https://placehold.co/600x400' : project.image } 
                     alt={`project image for ${project.title}`} 
-                    className="w-full h-full transition-[width_height] object-cover group-hover:h-[125%] group-hover:w-[125%]"
+                    className="w-full h-full transition-[width_height] object-cover object-center group-hover:h-[110%] group-hover:w-[110%]"
                 />
             </div>
             <div className="information px-4 py-3">
