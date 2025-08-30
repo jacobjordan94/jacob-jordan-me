21ad90a024ba92b692e4791f862e8a203f5cda48
Jacob Jordan <jacobjordan94@live.com>
2025-08-29 20:10:12 -0500

    add project-type-section component

---
+++ src/pages/projects/ProjectTypeSection.jsx
@@ -0,0 +1,23 @@
+import { AppAnchor } from "../../components/AppLink";
+import ProjectCard from "../../components/projects/ProjectCard";
+
+export default function ProjectTypeSection({ type, projects }) {
+    return (
+        <div className="project-type-section">
+            <div className="title-row text-3xl mb-6 flex items-center">
+                <div className="title">{ type }</div>
+                <div className="divider flex-grow opacity-25 ps-4"><hr /></div>
+            </div>
+            <div className="project-type-section-body flex flex-col gap-4 @2xl:grid @2xl:grid-cols-3">
+            {
+                projects.map((project, i) =>
+                    project.url ? 
+                    <AppAnchor key={i} className={'contents'} href={project.url}>
+                        <ProjectCard project={project} />
+                    </AppAnchor> :
+                    <ProjectCard key={i} project={project} />
+            )}
+            </div>
+        </div>
+    );
+}
