4b327e2da157d5707799699e5b112463be2b88f3
Jacob Jordan <jacobjordan94@live.com>
2025-08-29 20:15:37 -0500

    add projects top projects page

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,8 +1,16 @@
-export default function ProjectsPage({}) {
+import { useContext } from "react";
+import { GlobalContext } from '../../App';
+import ProjectTypeSection from "./ProjectTypeSection";
 
+export default function ProjectsPage({}) {
+    const { projects } = useContext(GlobalContext);
+    const ProjectTypes = () => [...new Set([...projects].map(project => project.type))]
+        .map((type, i) => <ProjectTypeSection key={i} type={type} projects={[...projects].filter(project => project.type === type)} />)
     return (
-        <div className="projects-page">
-            projects
+        <div className="projects-page flex flex-col gap-6">
+        {
+           <ProjectTypes />
+        }
         </div>
     );
 }
