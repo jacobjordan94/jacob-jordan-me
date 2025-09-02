f19d4ac7ecbc25fa5d5ed932471c358fcc903aab
Jacob Jordan <jacobjordan94@live.com>
2025-09-02 18:45:49 -0500

    add youtube videos to experience page

--- src/pages/experience/UPMCJob.jsx
+++ src/pages/experience/UPMCJob.jsx
@@ -1,9 +1,15 @@
+import YoutubeEmbedCard from "../../components/YoutubeEmbedCard";
+import YoutubeEmbedContainer from "../../components/YoutubeEmbedContainer";
 import Job from "./Job";
 
 export default function UPMCJob({ job }) {
     return  (
         <Job job={job}>
-            
+            <YoutubeEmbedContainer>
+                <YoutubeEmbedCard src="https://www.youtube.com/embed/hapeY6k6jC8" title="New App Aims To Address Rise Of Mental Health Issues In Teens"/>
+                <YoutubeEmbedCard src="https://www.youtube.com/embed/tQ-r6KKAuj4" title="Free phone app could help teens with depression and anxiety" />
+                <YoutubeEmbedCard src="https://youtube.com/embed/5Pu7xv6NcMo" title="What is RxWell?" />
+            </YoutubeEmbedContainer>
         </Job>
     );
 }
