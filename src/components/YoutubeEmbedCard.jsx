cc1950d49f527ef244162d0d97f3d46a889faaa3
Jacob Jordan <jacobjordan94@live.com>
2025-09-02 22:23:29 -0500

    fix some youtube embed errors

--- src/components/YoutubeEmbedCard.jsx
+++ src/components/YoutubeEmbedCard.jsx
@@ -1,9 +1,9 @@
 export default function YoutubeEmbedCard({ src = '', title = '', ...props }) {
     const defaultProps = {
-        frameborder: 0,
+        frameBorder: 0,
         allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
-        referrerpolicy: '',
-        allowfullscreen: true,
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        allowFullScreen: true,
     };
     return <div {...props} className={"youtube-card rounded-2xl overflow-hidden shadow-black shadow-md " + props.className}>
                <iframe src={src} title={title} { ...defaultProps } className="w-full aspect-video" />
