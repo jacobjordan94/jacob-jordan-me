d879f897907eca430cfd817594f5036d88b45564
Jacob Jordan <jacobjordan94@live.com>
2025-09-03 15:34:13 -0500

    add header icons to mobile side nav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -8,6 +8,7 @@ import PaintingIcon from "./icons/PaintingIcon";
 import ExperienceIcon from "./icons/ExperienceIcon";
 import GithubIcon from './icons/GithubIcon';
 import SourceCddeMagnifyingGlassIcon from './icons/SourceCodeMagnifyingGlassIcon';
+import { HeaderIcons } from "./Header";
 
 export default function SideNav({ open = false, setSideNavOpen }) {
 
@@ -45,14 +46,17 @@ export default function SideNav({ open = false, setSideNavOpen }) {
     return (
         <div className={"side-nav h-full w-full fixed z-10 top-0 left-0 flex bg-[rgba(0,0,0,0.5)] transition-[background-color] overflow-x-hidden " + (!open ? 'bg-transparent pointer-events-none' : '')}>
             <div className={"inner-menu w-0 bg-neutral-800 overflow-x-hidden transition-[width] " + (open ? 'w-[256px]' : '')}>
-                <div className="p-4 h-full relative">
-                    <button className="absolute py-3 px-5 top-0 right-0 cursor-pointer text-4xl" onClick={() => setSideNavOpen(false)}>&times;</button>
-                    <div className="navbody h-full flex flex-col flex-grow justify-between">
+                <div className="p-4 h-full flex flex-col">
+                    <div className="nav-header flex justify-end pe-2">                    
+                        <button className="float-right cursor-pointer text-4xl" onClick={() => setSideNavOpen(false)}>&times;</button>
+                    </div>
+                    <div className="navbody flex flex-col justify-between flex-grow">
                         <div className="top-navbody">
                             <NavBody items={navItems} currentPath={loc.pathname} />
                         </div>
                         <div className="bottom-navbody">
-                            <div className="bottom-navbody-text text-center text-md mb-4 flex items-center opacity-50">
+                            <HeaderIcons className={'pointer-coarse:visible justify-between'}/>
+                            <div className="bottom-navbody-text text-center text-md my-4 flex items-center opacity-50">
                                 <hr className="flex-grow" />
                                 <span className="mx-4 tracking-widest">more</span>
                                 <hr className="flex-grow"/>
