4170c9a474a385b2af246f7bf51f584faa918b3f
Jacob Jordan <jacobjordan94@live.com>
2025-09-22 22:02:37 -0500

    Add vite-plugin-sitemap plugin. Use to generate sitemap.xml and robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemapPlugin from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    sitemapPlugin({
+      hostname: 'https://jacob-jordan.me',
+      dynamicRoutes: [
+        '/about',
+        '/projects',
+        '/experience',
+        '/skills',
+        '/resume'
+      ],
+    })
+  ],
   server: {
     port: 3000,
     strictPort: true,
