f71274ff0d6cd8383a769f3495f2ee87e1752738
Jacob Jordan <jacobjordan94@live.com>
2025-09-22 22:46:54 -0500

    add ld+json support to seo component

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,3 +1,6 @@
+import { useContext, useMemo } from "react";
+import { GlobalContext } from '../App';
+
 /**
  * @typedef {Object} SeoProps
  * @property {string} title - The page title.
@@ -6,6 +9,7 @@
  * @property {string} [image] - Social image URL.
  * @property {'website'|'article'} [type='website'] - Open Graph type.
  * @property {string} [creator] - Twitter handle (optional).
+ * @property {Object} [schema] - Optional structured data object
  */
 
 /**
@@ -18,8 +22,50 @@ export default function Seo({
   image,
   type = 'website',
   creator,
+  schema,
 }) {
   const url = `https://jacob-jordan.me${pathname}`;
+  const { skills } = useContext(GlobalContext);
+  const defaultSchema = useMemo(() => ({
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Jacob A. Jordan",
+    url: "https://jacob-jordan.me",
+    sameAs: [
+      "https://github.com/jacobjordan94",
+      "https://www.linkedin.com/in/jacob-jordan-0ab56061/",
+      "https://discord.com/users/278942049917468684",
+    ],
+    jobTitle: "Front-End Web Developer",
+    // worksFor: {
+    //   "@type": "Organization",
+    //   name: "Could be you!"
+    // },
+    knowsAbout: [...skills],
+    contactPoint: {
+      "@type": "ContactPoint",
+      contactType: "Portfolio Contact Form",
+      url: "https://jacob-jordan.me",
+      email: "jacobjordan94@live.com", // If public, optional
+      availableLanguage: ["English"]
+    },
+    hasPart: [
+      {
+        "@type": "CreativeWork",
+        name: "Portfolio Website",
+        url: "https://jacob-jordan.me",
+        description: "Personal portfolio website built with React, Vite, and Tailwind CSS to showcase projects and experience.",
+        programmingLanguage: "JavaScript"
+      },
+      {
+        "@type": "CreativeWork",
+        name: "kexp-react",
+        url: "https://kexp-react.jacob-jordan.me",
+        description: "Fan site for KEXP.FM, a non-commercial radio station based in Seattle, WA. Made using React, Vite, & Tailwind",
+        programmingLanguage: "JavaScript"
+      },
+    ],
+  }), [ skills ]);
 
   return (
     <>
@@ -41,6 +87,10 @@ export default function Seo({
       <meta name="twitter:description" content={description} />
       {image && <meta name="twitter:image" content={image} />}
       {creator && <meta name="twitter:creator" content={creator} />}
+      <script 
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{__html: JSON.stringify(schema || defaultSchema)}}
+      />
     </>
   );
 }
